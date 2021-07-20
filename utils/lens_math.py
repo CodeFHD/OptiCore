0a778068d6d0231b72a922cf5bdfd053e564a2d0
CodeFHD <johannes@hinrichs-hosen.de>
2021-07-20 19:47:16 +0200

    fix divide by zero in lensmakers equation

--- utils/lens_math.py
+++ utils/lens_math.py
@@ -1,6 +1,27 @@
 def f_lensmaker(r1,r2,n, d):
-    dummy = (n-1) * (1./r1 - 1./r2 + (n-1)*d/(n*r1*r2))
-    return 1/dummy
+    if r1 == 0:
+        invr1 = 0
+    else:
+        invr1 = 1./r1
+
+    if r2 == 0:
+        invr2 = 0
+    else:
+        invr2 = 1./r2
+
+    if r1 == 0 or r2 == 0:
+        invr1r2 = 0
+    else:
+        invr1r2 = 1./(r1*r2)
+
+    dummy = (n-1) * (invr1 - invr2 + (n-1)*d*invr1r2/n)
+
+    if dummy == 0:
+        invdummy = 0
+    else:
+        invdummy = 1./dummy
+    
+    return invdummy
 
 
 
