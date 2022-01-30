ba1fc610f13e9698a205a40130671a426a211995
CodeFHD <johannes@hinrichs-hosen.de>
2022-01-30 17:44:21 +0100

    Update of retroreflector array

--- elements/ccretro.py
+++ elements/ccretro.py
@@ -59,9 +59,10 @@ class OBJECT_OT_add_CCretro(Operator, AddObjectHelper):
            )
     ccdist : FloatProperty(
            name="CubeCorner Spacing",
-           default = 1.,
+           default = 3.,
            description="Distance between CubeConer cnetres",
            unit = "LENGTH",
+           min=0,
            )
     facethickness : FloatProperty(
            name="Face Thickness",
@@ -145,6 +146,14 @@ def add_ccretro_cube(self, context):
     fthick = self.facethickness
     A = self.A
 
+    #sanity check
+    if ccdist > min(rlenx, rleny)/3: # avoid too few CC elements, makes geo issues more complex to deal with
+        ccdist = min(rlenx, rleny)/3
+    if ccdist < max(rlenx, rleny)/100: #avoid too high number of cc elements becasue of performance
+        ccdist = max(rlenx, rleny)/100
+
+    A = [A[i]*ccdist/20 for i in range(len(A))]
+
     #calculate base parameters
     h = ccdist/2
     dyrow = ccdist*np.sqrt(3)/2 #y offset betwee rows
@@ -152,16 +161,14 @@ def add_ccretro_cube(self, context):
     rpts = ccdist/np.sqrt(3) #radius of corner points around center
     dz = ccdist/np.sqrt(6) #height offset of points, alternate dz and 2*dz
 
-    ccdepth = 2*dz #total depth of cube corner
-
-    A = [A[i]*ccdist/20 for i in range(len(A))]
+    ccdepth = 2*dz #total depth of cube corner - not including tip offset!
 
     #calc number of cc elements:
     if self.rshape == 'circular':
         pass
     elif self.rshape == 'rectangular':
-        nx = int(np.floor(rlenx/ccdist - 0.2))
-        ny = int(np.floor(rleny/dyrow - 0.2))
+        nx = int(np.floor(rlenx/ccdist - 0.3))
+        ny = int(np.floor(rleny/dyrow - 0.3))
         if ny%2 == 0:
             ny = ny-1
         ccxoff = -(nx-1)*ccdist/2
@@ -170,21 +177,21 @@ def add_ccretro_cube(self, context):
     #sanity checks
     if fthick < 0:
         facdir = -1
-        if fthick > -1.01*ccdepth:
-            fthick = -1.01*ccdepth
+        if fthick > -1.01*(ccdepth - A[2]):
+            fthick = -1.01*(ccdepth - A[2])
     else:
         facdir = 1
-        if fthick < 0.01*ccdepth:
-            fthick = 0.01*ccdepth
+        if fthick < 0.01*(ccdepth - A[2]):
+            fthick = 0.01*(ccdepth - A[2])
 
     #make outline verts
     if self.rshape == 'circular':
         pass
     elif self.rshape == 'rectangular':
-        verts.append(Vector((-rlenx/2,-rleny/2,fthick)))
-        verts.append(Vector((-rlenx/2,rleny/2,fthick)))
-        verts.append(Vector((rlenx/2,rleny/2,fthick)))
-        verts.append(Vector((rlenx/2,-rleny/2,fthick)))
+        verts.append(Vector((-rlenx/2,-rleny/2,fthick+A[2])))
+        verts.append(Vector((-rlenx/2,rleny/2,fthick+A[2])))
+        verts.append(Vector((rlenx/2,rleny/2,fthick+A[2])))
+        verts.append(Vector((rlenx/2,-rleny/2,fthick+A[2])))
 
         verts.append(Vector((-rlenx/2,-rleny/2,0)))
         verts.append(Vector((-rlenx/2,rleny/2,0)))
@@ -198,19 +205,42 @@ def add_ccretro_cube(self, context):
         for i in range(nx - j%2):
             x = ccxoff + i*ccdist + dxrow*(j%2) + A[0]
             y = ccyoff + j*dyrow + A[1]
-            z = -ccdepth + A[2]
+            z = -ccdepth + 2*A[2]
             verts.append(Vector((x,y,z)))
     ntips = ny*nx - ny//2
 
     #make cc top verts (in rows+1)
     for j in range(ny+1):
         for i in range(2*nx + 1):
-            x = ccxoff + h*(i-1)
-            y = ccyoff + j*dyrow - rpts/2*(1 + (i+j)%2)
-            z = -1*((i+j-1)%2)*dz
+            vsel = ((i+j+1)%2)==0 #in every second row, every second vertex is the top tip.
+            x = ccxoff + h*(i-1) - vsel*A[0]
+            y = ccyoff + j*dyrow - rpts/2*(1 + (i+j)%2) - vsel*A[1]
+            z = -1*((i+j-1)%2)*dz  + A[2] - vsel*A[2]
             verts.append(Vector((x,y,z)))
 
+    nCVerts = len(verts)
+
+    #make raised end verts (straight up as if it were a pingrid array)
+    #left
+    for j in range(ny+1):
+        o = noutline + ntips + (2*nx + 1)*j + j%2
+        verts.append(Vector((verts[o][0],verts[o][1],0)))
+    #right
+    for j in range(ny+1):
+        o = noutline + ntips + (2*nx + 1)*(j+1) - 1 - j%2
+        verts.append(Vector((verts[o][0],verts[o][1],0)))
+    #bottom
+    j = 0
+    for i in range(2*nx + 1)[2:-1:2]:
+        o = noutline + ntips + (2*nx + 1)*j + i
+        verts.append(Vector((verts[o][0],verts[o][1],0)))
+    #top
+    j = ny
+    for i in range(2*nx + 1)[3:-2:2]:
+        o = noutline + ntips + (2*nx + 1)*j + i
+        verts.append(Vector((verts[o][0],verts[o][1],0)))
     
+
     #fill faces
     
     #front and side faces
@@ -219,89 +249,7 @@ def add_ccretro_cube(self, context):
     faces.append([1,2,6,5])
     faces.append([2,3,7,6])
     faces.append([3,0,4,7])
-
-    #interface
-    #side1
-    faces.append([7,4] + [noutline + ntips + 2*i + 1 for i in range(nx)])
-    o = noutline + ntips
-    for i in range(nx - 1):
-        f1 = o + 2*i + 1
-        f2 = o + 2*i + 2
-        f3 = o + 2*i + 3
-        faces.append([f1,f2,f3])
-    #side2
-    faces.append([4,5] + [noutline + ntips + (2*nx + 1)*(j+1) for j in range(ny)[::2]][::-1])
-    for j in range(ny):
-        if j%2 == 0:
-            continue
-        o = noutline + ntips + (2*nx + 1)*j
-        f1 = o
-        f2 = o + (2*nx + 1)*2
-        f3 = o + (2*nx + 1) + 1
-        faces.append([f1,f2,f3])
-        
-        f1 = o + 1
-        f2 = o 
-        f3 = o + (2*nx + 1) + 1
-        faces.append([f1,f2,f3])
-        
-        f1 = o + (2*nx + 1)*2
-        f2 = o + (2*nx + 1)
-        f3 = o + (2*nx + 1) + 1
-        faces.append([f1,f2,f3])
-    #side3
-    o = noutline + ntips + (2*nx + 1)*ny
-    faces.append([5,6] + [o + 2*i for i in range(nx + 1)][::-1])
-    for i in range(nx):
-        o = noutline + ntips + (2*nx + 1)*ny + 2*i
-        f1 = o
-        f2 = o + 2
-        f3 = o + 1
-        faces.append([f1,f2,f3])
-    #side4
-    faces.append([6,7] + [noutline + ntips + (2*nx + 1)*(j+1) - 1 for j in range(ny + 1)[1::2]])
-    for j in range(ny):
-        if j%2 == 0:
-            continue
-        o = noutline + ntips + (2*nx + 1)*j
-        f1 = o + (2*nx + 1)*2 -2
-        f2 = o + (2*nx + 1)*3 -1
-        f3 = o + (2*nx + 1) - 1
-        faces.append([f1,f2,f3])
-        
-        f1 = o - 2
-        f2 = o - 1
-        f3 = o + (2*nx + 1) - 1
-        faces.append([f1,f2,f3])
-        
-        f1 = o + (2*nx + 1)*2 - 2
-        f2 = o + (2*nx + 1) - 1
-        f3 = o + (2*nx + 1) - 2
-        faces.append([f1,f2,f3])
-    #close off corners
-    #fl
-    o = noutline + ntips
-    f1 = o
-    f2 = o + 1
-    f3 = o + (2*nx + 1)
-    faces.append([f1,f2,f3])
-    f1 = 4
-    f2 = o + (2*nx + 1)
-    f3 = o + 1
-    faces.append([f1,f2,f3])
-    #fr
-    o = noutline + ntips + (2*nx + 1)
-    f1 = 7
-    f2 = o - 2
-    f3 = o + (2*nx + 1) - 1
-    faces.append([f1,f2,f3])
-    #rr
-    o = noutline + ntips + (2*nx + 1)*(ny)
-    f1 = o - 2
-    f2 = o - 1
-    f3 = o + (2*nx + 1) - 1
-    faces.append([f1,f2,f3])
-
+    
     #cubes
     for j in range(ny):
         for i in range(nx - j%2):
@@ -325,6 +273,150 @@ def add_ccretro_cube(self, context):
             f3 = o3
             f4 = o3 + 1
             faces.append([f1,f2,f3,f4])
+
+    #pingrid sides
+    #left
+    for j in range(ny+1):
+        k = j+1
+        l = 1-2*(j%2)
+        f1 = nCVerts + j
+        f2 = noutline + ntips + (2*nx + 1)*j + j%2
+        f3 = f2 + l
+        faces.append([f1,f2,f3])
+        f1 = nCVerts + j
+        f2 = noutline + ntips + (2*nx + 1)*j + j%2
+        if j < ny:
+            f3 = noutline + ntips + (2*nx + 1)*k + j%2
+        else:
+            f3 = f2+1
+        faces.append([f1,f3,f2])
+    #right
+    for j in range(ny+1):
+        k = j+1
+        l = 1-2*(j%2)
+        f1 = nCVerts + ny + 1 + j
+        f2 = noutline + ntips + (2*nx + 1)*(j+1) - 1 - j%2
+        f3 = f2 - l
+        faces.append([f1,f3,f2])
+        f1 = nCVerts + ny + 1 + j
+        f2 = noutline + ntips + (2*nx + 1)*(j+1) - 1 - j%2
+        if j < ny:
+            f3 = noutline + ntips + (2*nx + 1)*(k+1) - 1 - j%2
+        else:
+            f3 = f2-1
+        faces.append([f1,f2,f3])
+    #bottom
+    j = 0
+    i2 = -1
+    for i in range(2*nx + 1)[2:-1:2]:
+        i2 += 1
+        o = noutline + ntips + (2*nx + 1)*j + i
+        f1 = nCVerts + 2*ny + 2 + i2
+        f2 = o
+        f3 = o-1
+        faces.append([f1,f3,f2])
+        f1 = nCVerts + 2*ny + 2 + i2
+        f2 = o
+        f3 = o+1
+        faces.append([f1,f2,f3])
+    #top
+    j = ny
+    i2 = -1
+    nx2 = len(range(2*nx + 1)[2:-1:2])
+    for i in range(2*nx + 1)[3:-2:2]:
+        i2 += 1
+        o = noutline + ntips + (2*nx + 1)*j + i
+        f1 = nCVerts + 2*ny + 2 + nx2 + i2
+        f2 = o
+        f3 = o-1
+        faces.append([f1,f2,f3])
+        f1 = nCVerts + 2*ny + 2 + nx2 + i2
+        f2 = o
+        f3 = o+1
+        faces.append([f1,f3,f2])
+
+    
+
+    #interface
+    #left
+    f1 = [noutline + ntips + (2*nx + 1)*j for j in range(ny+1)[1::2]]
+    f2 = [nCVerts + j for j in range(ny+1)[::2]]
+    f = [None]*(len(f1)+len(f2))
+    f[::2] = f2
+    f[1::2] = f1
+    faces.append([4,5]+f[::-1])
+    #right
+    f1 = [noutline + ntips + (2*nx + 1)*j - 1 for j in range(1,ny+2)[1::2]]
+    f2 = [nCVerts + ny+1 + j for j in range(ny+1)[::2]]
+    f = [None]*(len(f1)+len(f2))
+    f[::2] = f2
+    f[1::2] = f1
+    faces.append([6,7]+f)
+    #bottom
+    f = [noutline + ntips + i for i in range(2*nx+1)[1::2]]
+    faces.append([7,4]+f)
+    #top
+    f = [nCVerts + 2*ny + nx + 1 + i for i in range(nx-2)]
+    f = [nCVerts + ny] + f + [nCVerts + 2*ny+1]
+    faces.append([5,6]+f[::-1])
+
+    #intermediates
+    #left
+    for j in range(int((ny-1)/2)):
+        f1 = noutline + ntips + (2*nx+1)*(2*j+1)
+        f2 = nCVerts + 2*j + 2
+        f3 = noutline + ntips + (2*nx+1)*(2*j+2) + 1
+        f4 = nCVerts + 2*j + 1
+        faces.append([f1,f2,f3,f4])
+    #right
+    for j in range(int((ny-1)/2)):
+        f1 = noutline + ntips + (2*nx+1)*(2*j+2) - 1
+        f2 = nCVerts + ny+1 + 2*j + 2
+        f3 = noutline + ntips + (2*nx+1)*(2*j+3) - 2
+        f4 = nCVerts + ny+1 + 2*j + 1
+        faces.append([f4,f3,f2,f1])
+    #bottom
+    for i in range(nx-1):
+        f1 = nCVerts + 2*ny + 2 + i
+        f2 = noutline + ntips + 2*i + 3
+        f3 = noutline + ntips + 2*i + 1
+        faces.append([f1,f2,f3])
+    #top
+    f1 = nCVerts + ny
+    f2 = nCVerts + 2*ny+1 + nx
+    f3 = noutline + ntips + (2*nx + 1)*ny + 2
+    faces.append([f1,f2,f3])
+    for i in range(nx-3):
+        f1 = nCVerts + 2*ny+1 + nx + i
+        f2 = nCVerts + 2*ny+1 + nx + i+1
+        f3 = noutline + ntips + (2*nx + 1)*ny + 4 + 2*i
+        faces.append([f1,f2,f3])
+    f1 = nCVerts + 2*ny+1 + 2*nx -3
+    f2 = nCVerts + 2*ny+1
+    f3 = noutline + ntips + (2*nx + 1)*ny + 2*nx - 2
+    faces.append([f1,f2,f3])
+
+    #corners
+    j = 0
+    f1 = 4
+    f3 = noutline + ntips + (2*nx + 1)*j + j%2 + 1
+    f2 = nCVerts
+    faces.append([f1,f2,f3])
+    j = ny
+    f1 = 5
+    f2 = nCVerts + ny
+    f3 = noutline + ntips + (2*nx + 1)*j + j%2 - 1
+    faces.append([f1,f2,f3])
+    j = ny +1
+    f1 = 6
+    f2 = nCVerts + 2*ny +1
+    f3 = noutline + ntips + (2*nx + 1)*j + j%2 - 1
+    faces.append([f1,f3,f2])
+    j = 1
+    f1 = 7
+    f2 = nCVerts + ny +1
+    f3 = noutline + ntips + (2*nx + 1)*j + j%2 - 3
+    faces.append([f1,f3,f2])
 
             
 
