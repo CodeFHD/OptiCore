7341553374ffc5db0b72fb7afacdce5422071260
CodeFHD <joho.hini@gmail.com>
2025-02-28 15:01:44 +0100

    udpate __init__.py from development branch

--- __init__.py
+++ __init__.py
@@ -53,112 +53,111 @@ if in_blender:
         def draw(self, context):
             pass
 
-    def execute(self, context):
-        reset_lens_defaults()
-        reset_loadzmx_faults()
-        return {'FINISHED'}
+        def execute(self, context):
+            reset_lens_defaults()
+            reset_loadzmx_faults()
+            return {'FINISHED'}
 
-class OBJECT_MT_opticsmenu(bpy.types.Menu):
-    bl_idname = 'mesh.opticsmenu'
-    bl_label = 'OptiCore Optics'
+    class OBJECT_MT_opticsmenu(bpy.types.Menu):
+        bl_idname = 'OBJECT_MT_opticsmenu'
+        bl_label = 'OptiCore Optics'
 
-    def draw(self, context):
-        self.layout.operator(OBJECT_OT_add_lens.bl_idname)
-        # self.layout.operator(OBJECT_OT_add_sqlens.bl_idname)
-        self.layout.operator(OBJECT_OT_add_mirror.bl_idname)
-        self.layout.operator(OBJECT_OT_add_CCretro.bl_idname)
-        self.layout.operator(OBJECT_OT_add_siemens.bl_idname)
-        self.layout.operator(OBJECT_OT_load_zmx.bl_idname)
-        # self.layout.operator(OBJECT_OT_test_zmx.bl_idname)
-        self.layout.operator(OBJECT_OT_reset_performance_variables.bl_idname)
+        def draw(self, context):
+            self.layout.operator(OBJECT_OT_add_lens.bl_idname)
+            # self.layout.operator(OBJECT_OT_add_sqlens.bl_idname)
+            self.layout.operator(OBJECT_OT_add_mirror.bl_idname)
+            self.layout.operator(OBJECT_OT_add_CCretro.bl_idname)
+            self.layout.operator(OBJECT_OT_add_siemens.bl_idname)
+            self.layout.operator(OBJECT_OT_load_zmx.bl_idname)
+            # self.layout.operator(OBJECT_OT_test_zmx.bl_idname)
+            self.layout.operator(OBJECT_OT_reset_performance_variables.bl_idname)
 
-class OBJECT_MT_optomechmenu(bpy.types.Menu):
-    bl_idname = 'mesh.optomechmenu'
-    bl_label = 'OptiCore Optomechanics'
+    class OBJECT_MT_optomechmenu(bpy.types.Menu):
+        bl_idname = 'OBJECT_MT_optomechmenu'
+        bl_label = 'OptiCore Optomechanics'
 
-    def draw(self, context):
-        self.layout.operator(OBJECT_OT_add_table.bl_idname)
-        self.layout.operator(OBJECT_OT_add_post.bl_idname)
-    
-def menu_func(self, context):
-    self.layout.separator()
-    self.layout.menu(OBJECT_MT_opticsmenu.bl_idname)
-    self.layout.menu(OBJECT_MT_optomechmenu.bl_idname)
+        def draw(self, context):
+            self.layout.operator(OBJECT_OT_add_table.bl_idname)
+            self.layout.operator(OBJECT_OT_add_post.bl_idname)
+        
+    def menu_func(self, context):
+        self.layout.separator()
+        self.layout.menu(OBJECT_MT_opticsmenu.bl_idname)
+        self.layout.menu(OBJECT_MT_optomechmenu.bl_idname)
 
-# Registration
+    # Registration
 
-def add_lens_button(self, context):
-    self.layout.operator(
-        OBJECT_OT_add_lens.bl_idname,
-        text="Add Lens",
-        icon='PLUGIN')
-"""
-def add_lsqens_button(self, context):
-    self.layout.operator(
-        OBJECT_OT_add_sqlens.bl_idname,
-        text="Add Square Lens",
-        icon='PLUGIN')
-"""
-def add_mirror_button(self, context):
-    self.layout.operator(
-        OBJECT_OT_add_mirror.bl_idname,
-        text="Add Mirror",
-        icon='PLUGIN')
-def add_table_button(self, context):
-    self.layout.operator(
-        OBJECT_OT_add_table.bl_idname,
-        text="Add Table",
-        icon='PLUGIN')
-def load_zmx_button(self, context):
-    self.layout.operator(
-        OBJECT_OT_load_zmx.bl_idname,
-        text="Load lenses from .zmx file",
-        icon='PLUGIN')
-"""
-def test_zmx_button(self, context):
-    self.layout.operator(
-        OBJECT_OT_test_zmx.bl_idname,
-        text="Test zmx library",
-        icon='PLUGIN')
-"""
-def reset_performance_button(self, context):
-    self.layout.operator(
-        OBJECT_OT_reset_performance_variables.bl_idname,
-        text="Reset performance critical variables",
-        icon='PLUGIN')
+    def add_lens_button(self, context):
+        self.layout.operator(
+            OBJECT_OT_add_lens.bl_idname,
+            text="Add Lens",
+            icon='PLUGIN')
+    """
+    def add_lsqens_button(self, context):
+        self.layout.operator(
+            OBJECT_OT_add_sqlens.bl_idname,
+            text="Add Square Lens",
+            icon='PLUGIN')
+    """
+    def add_mirror_button(self, context):
+        self.layout.operator(
+            OBJECT_OT_add_mirror.bl_idname,
+            text="Add Mirror",
+            icon='PLUGIN')
+    def add_table_button(self, context):
+        self.layout.operator(
+            OBJECT_OT_add_table.bl_idname,
+            text="Add Table",
+            icon='PLUGIN')
+    def load_zmx_button(self, context):
+        self.layout.operator(
+            OBJECT_OT_load_zmx.bl_idname,
+            text="Load lenses from .zmx file",
+            icon='PLUGIN')
+    """
+    def test_zmx_button(self, context):
+        self.layout.operator(
+            OBJECT_OT_test_zmx.bl_idname,
+            text="Test zmx library",
+            icon='PLUGIN')
+    """
+    def reset_performance_button(self, context):
+        self.layout.operator(
+            OBJECT_OT_reset_performance_variables.bl_idname,
+            text="Reset performance critical variables",
+            icon='PLUGIN')
 
-"""
-# This allows you to right click on a button and link to the manual
-def add_lens_manual_map():
-    url_manual_prefix = "https://docs.blender.org/manual/en/dev/"
-    url_manual_mapping = (
-        ("bpy.ops.mesh.add_lens", "editors/3dview/object"),
-        )
-    return url_manual_prefix, url_manual_mapping
-def add_mirror_manual_map():
-    url_manual_prefix = "https://docs.blender.org/manual/en/dev/"
-    url_manual_mapping = (
-        ("bpy.ops.mesh.add_mirror", "editors/3dview/object"),
-        )
-    return url_manual_prefix, url_manual_mapping
-"""
+    """
+    # This allows you to right click on a button and link to the manual
+    def add_lens_manual_map():
+        url_manual_prefix = "https://docs.blender.org/manual/en/dev/"
+        url_manual_mapping = (
+            ("bpy.ops.mesh.add_lens", "editors/3dview/object"),
+            )
+        return url_manual_prefix, url_manual_mapping
+    def add_mirror_manual_map():
+        url_manual_prefix = "https://docs.blender.org/manual/en/dev/"
+        url_manual_mapping = (
+            ("bpy.ops.mesh.add_mirror", "editors/3dview/object"),
+            )
+        return url_manual_prefix, url_manual_mapping
+    """
 
-classes = (OBJECT_OT_reset_performance_variables,
-           OBJECT_OT_add_lens, OBJECT_OT_add_mirror, OBJECT_OT_add_CCretro, # , OBJECT_OT_add_sqlens
-           OBJECT_OT_add_siemens, OBJECT_OT_load_zmx, # OBJECT_OT_test_zmx,
-          OBJECT_OT_add_table,OBJECT_OT_add_post)
-menus = (OBJECT_MT_opticsmenu, OBJECT_MT_optomechmenu)
+    classes = (OBJECT_OT_reset_performance_variables,
+            OBJECT_OT_add_lens, OBJECT_OT_add_mirror, OBJECT_OT_add_CCretro, # , OBJECT_OT_add_sqlens
+            OBJECT_OT_add_siemens, OBJECT_OT_load_zmx, # OBJECT_OT_test_zmx,
+            OBJECT_OT_add_table,OBJECT_OT_add_post)
+    menus = (OBJECT_MT_opticsmenu, OBJECT_MT_optomechmenu)
 
-def register():
-    for cla in classes+menus:
-        print('REGISTERING',  cla)
-        bpy.utils.register_class(cla)
-    bpy.types.VIEW3D_MT_mesh_add.append(menu_func)
-    
-def unregister():
-    for cla in classes+menus:
-        bpy.utils.unregister_class(cla)
-    bpy.types.VIEW3D_MT_mesh_add.remove(menu_func)
+    def register():
+        for cla in classes+menus:
+            bpy.utils.register_class(cla)
+        bpy.types.VIEW3D_MT_mesh_add.append(menu_func)
+        
+    def unregister():
+        for cla in classes+menus:
+            bpy.utils.unregister_class(cla)
+        bpy.types.VIEW3D_MT_mesh_add.remove(menu_func)
 
 if __name__ == "__main__":
     register()
